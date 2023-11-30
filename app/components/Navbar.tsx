702fbef5f6705eefe2d20d2796d180fc4fda6535
Karl George <karlgeorge87@gmail.com>
2023-11-30 14:52:47 +0100

    Add genre dropdown

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,26 +1,57 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { Genre } from '@/type';
+import { getGenres } from '@/lib/actions';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 interface NavbarProps {}
 
-function Navbar({}: NavbarProps) {
+async function Navbar({}: NavbarProps) {
+  const { genres } = await getGenres();
+
   return (
     <header className='bg-[#131313] w-full'>
-      <nav className='flex justify-between items-center container py-2'>
+      <nav className='flex justify-between items-center container py-3'>
         {/* Logo */}
         <div>
-          <Image
-            src={'/logo.png'}
-            width={150}
-            height={150}
-            alt='MoviFind logo'
-          />
+          <Link href={'/'}>
+            <Image
+              src={'/logo.png'}
+              width={150}
+              height={150}
+              alt='MoviFind logo'
+            />
+          </Link>
         </div>
         {/* Navbar dropdown for genre country year */}
         <div>
-          <ul className='flex justify-center items-center gap-12 text-textParagraph'>
-            <li>Genre</li>
-            <li>Country</li>
+          <ul className='flex justify-center items-center gap-12 text-textParagraph text-lg'>
+            <li>
+              <DropdownMenu>
+                <DropdownMenuTrigger className='border-none focus:none'>
+                  Genre
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className='w-96 flex flex-row flex-wrap  gap-4'>
+                  {genres.map((genre: Genre) => (
+                    <DropdownMenuItem key={genre.id}>
+                      <Link
+                        href={'#'}
+                        className='text-textParagraph hover:text-textHeadline text-base'
+                      >
+                        {genre.name}
+                      </Link>
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </li>
             <li>Year</li>
+            <li>Country</li>
           </ul>
         </div>
         {/* Search Login */}
