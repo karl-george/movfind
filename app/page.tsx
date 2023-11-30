c52413e07694085349656186e525320fc27a13dc
Karl George <karlgeorge87@gmail.com>
2023-11-30 12:19:45 +0100

    Add fetch movies by country

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import { getGenres, getMovies, getMoviesByYearRange } from '@/lib/actions';
+import {
+  getGenres,
+  getMovies,
+  getMoviesByCountry,
+  getMoviesByYearRange,
+} from '@/lib/actions';
 
 export default async function Home() {
   const data = await getMovies();
   const { genres } = await getGenres();
-  const { results } = await getMoviesByYearRange('1990', '2000');
+  // const { results } = await getMoviesByYearRange('1990', '2000');
+  const { results } = await getMoviesByCountry('cn');
 
   return (
     <main>
@@ -16,6 +22,9 @@ export default async function Home() {
       {/* {results.map((moviebyYear: any) => (
         <div>{moviebyYear.title}</div>
       ))} */}
+      {results.map((moviebyCountry: any) => (
+        <div>{moviebyCountry.title}</div>
+      ))}
     </main>
   );
 }
--- lib/actions.ts
+++ lib/actions.ts
@@ -49,6 +49,29 @@ export const getMoviesByYearRange = async (
   }
 };
 
+export const getMoviesByCountry = async (country: string) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.MOVIE_API_KEY}`,
+    },
+  };
+
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&page=1&sort_by=popularity.desc&with_origin_country=${country}&with_original_language=${country}`,
+      options
+    );
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to fetch movies by year');
+  }
+};
+
 export const getGenres = async () => {
   const options = {
     method: 'GET',
