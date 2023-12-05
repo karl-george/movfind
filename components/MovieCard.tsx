d9138aa4b82652a30230fd36df5b17ac155c5727
Karl George <karlgeorge87@gmail.com>
2023-12-05 10:47:30 +0100

    Add animations

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { MotionDiv } from './MotionDiv';
 
 export interface MovieProp {
   adult: boolean;
@@ -23,19 +24,33 @@ interface MovieCardProps {
   index: number;
 }
 
+const variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+
 function MovieCard({ movie, index }: MovieCardProps) {
   return (
-    <div className='relative'>
-      <Link href={`/movie/${movie.id}`}>
-        <Image
-          width={360}
-          height={550}
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-          className='object-contain rounded-2xl'
-        />
-      </Link>
-    </div>
+    <MotionDiv
+      variants={variants}
+      initial='hidden'
+      animate='visible'
+      transition={{ duration: 0.5, delay: index * 0.25, ease: 'easeInOut' }}
+      viewport={{ amount: 0 }}
+    >
+      <div className='relative'>
+        <Link href={`/movie/${movie.id}`}>
+          <Image
+            width={360}
+            height={550}
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            className='object-contain rounded-2xl'
+          />
+          <p className='text-white'>{index}</p>
+        </Link>
+      </div>
+    </MotionDiv>
   );
 }
 
